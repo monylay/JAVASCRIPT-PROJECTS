fbe6eebc58f53bf2c225ea4cd46bdc1a3193d6e5
monylay <monylay48@gmail.com>
2026-05-26 12:42:24 -0400

    practice

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,32 @@
+var A = "This will start \"This is string One\" and \"This is String Two\"";
+document.write(A);
+
+var B = "Hello, World!";
+window.alert(B);
+
+var C = "\"Be who you are and say what you feel,"
++ "because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. Seuss"; //testing concatenation and \
+document.write(C);
+
+var Family = "The PoGoPe", Mom = "Pomi", Son = "Goldie", Daughter = "Pearl"; //Family Tree
+document.write(Mom);
+
+3 + 3
+
+document.write(3 + 3);
+
+function My_First_Function() {
+    var str = "This is the button text!";
+    document.getElementById("Button_Text").innerHTML = str;
+}
+
+
+var Sent1 = "This is the beginning of the string"; // test string 1
+document.write(Sent1);
+
+var Sent2 = "and this is the end of the string"; //test string 2
+document.write(Sent2);
+
+var Sent3 = "This is the beginning of the string" + "" +" and this is the end of the string";
+document.write(Sent3); //concatenate string 1 and 2
